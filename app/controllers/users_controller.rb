42f33df80683f5e7e8658cce0082e26018e9ee7c
jodat1 <jodat.jabal1@gmail.com>
2022-05-30 18:02:43 +0500

    added login and created sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController <ApplicationController
+    
+    def index
+        @users = User.all
+    end
+    
     def new
 
         @user = User.new
@@ -8,6 +13,8 @@ class UsersController <ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+
+            session[:user_id] = @user_id
             flash[:success] = "Welcome #{@user.username}"
             redirect_to articles_path
         else
